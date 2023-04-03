0f5bdddd680a2109508058051929e2e7e58c3922
sinansaricayir <sinansaricayir@gmail.com>
2023-04-03 03:43:59 +0300

    React Router DOM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,15 @@
-import CartTotals from "./components/cart/CartTotals";
-import Categories from "./components/categories/Categories";
-import Header from "./components/header/Header";
-import Products from "./components/products/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10">
-        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] pb-10">
-          <Categories />
-        </div>
-        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products />
-        </div>
-        <div className="cart-totals min-w-[300px] md:-mr-[24px] md:-mt-[24px] md:pb-0 pb-12">
-          <CartTotals />
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import CartTotals from "../components/cart/CartTotals";
+import Categories from "../components/categories/Categories";
+import Header from "../components/header/Header";
+import Products from "../components/products/Products";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10">
+        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] pb-10">
+          <Categories />
+        </div>
+        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
+          <Products />
+        </div>
+        <div className="cart-totals min-w-[300px] md:-mr-[24px] md:-mt-[24px] md:pb-0 pb-12">
+          <CartTotals />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
