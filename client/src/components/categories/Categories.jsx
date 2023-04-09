f8449444179daa402db05a23ac30bc0fbdbd6e6f
sinansaricayir <sinansaricayir@gmail.com>
2023-04-09 19:27:49 +0300

    Category Client & Api | GET - POST

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,33 +1,69 @@
-const Categories = () => {
+import "./style.css";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Modal, message } from "antd";
+import { useState } from "react";
+
+const Categories = ({ categories, setCategories }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  const onFinish = (value) => {
+    try {
+      fetch("http://localhost:4000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(value),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      message.success("Kategori başarıyla eklendi.");
+      setIsModalOpen(false);
+      form.resetFields();
+      setCategories([...categories, value]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <ul className="flex gap-4 md:flex-col flex-row text-lg overflow-x-auto">
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>Tümü</span>
+      {categories.map((item) => (
+        <li className="category-item">
+          <span>{item.title}</span>
+        </li>
+      ))}
+
+      <li className="category-item  !bg-purple-800 hover:opacity-90">
+        <PlusOutlined
+          className="md:text-3xl text-2xl"
+          onClick={() => setIsModalOpen(true)}
+        />
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>Yiyecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
-      <li className="bg-green-700 px-6 py-10 text-white text-center cursor-pointer hover:bg-pink-700 transition-all min-w-[145px]">
-        <span>İçecek</span>
-      </li>
+
+      <Modal
+        title="Yeni Kategori Ekle"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item
+            label={"Kategori Ekle"}
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: "Bu Alan Boş Geçilemez!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Oluştur
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </ul>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,15 +1,32 @@
+import { useState, useEffect } from "react";
 import CartTotals from "../components/cart/CartTotals";
 import Categories from "../components/categories/Categories";
 import Header from "../components/header/Header";
 import Products from "../components/products/Products";
 
 function HomePage() {
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/api/categories/get-all");
+      const data = await res.json();
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10">
         <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] pb-10">
-          <Categories />
+          <Categories categories={categories} setCategories={setCategories} />
         </div>
         <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
           <Products />
